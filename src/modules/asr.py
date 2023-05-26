7d5c55f48fd9566ef1fc4023032cc636bbb41f61
Dandan <daniel.leongsj@gmail.com>
2023-05-26 11:44:12 +0800

    add temporary ray-container method

---
+++ src/modules/asr.py
@@ -0,0 +1,92 @@
+# from https://github.com/DinoHub/peeps/blob/ray-pipeline/ray_pipeline/detector/detector.py
+
+import logging
+from typing import Any, List, Optional, Union
+
+import ray
+from ray import serve
+
+from config import config, BaseConfig
+
+LOGGER = logging.getLogger('ray.serve')
+RUNTIME_ENV = {
+    'container': {
+        'image': 'dleongsh/ray-dory:v1.18.0',
+        'run_options': ['--runtime=/usr/bin/nvidia-container-runtime']
+    }
+}
+
+@serve.deployment(ray_actor_options={"num_cpus": config.num_cpus, "num_gpus": config.num_gpus})
+class Transcriber:
+
+    def __init__(self, logging_level=logging.WARNING) -> ASRModel:
+
+        from starlette.requests import Request
+
+        import os
+        import base64
+        import logging
+
+        import torch
+        import librosa
+        import numpy as np
+        import gradio as gr
+        import soundfile as sf
+        import pytorch_lightning as pl
+        from nemo.utils import model_utils
+        from nemo.collections.asr.models import ASRModel
+
+        DEVICE: Union[List[int], int] = [0] if (torch.cuda.is_available() and config.num_gpus >= 1) else 1
+        ACCELERATOR: str = 'gpu' if (torch.cuda.is_available() and config.num_gpus >= 1) else 'cpu'
+        MAP_LOCATION: str = torch.device(f'cuda:{DEVICE[0]}') if ACCELERATOR == 'gpu' else 'cpu'
+
+        # Load model
+        model_cfg = ASRModel.restore_from(restore_path=config.asr_model_path, return_config=True)
+        classpath = model_cfg.target  # original class path
+        imported_class = model_utils.import_class_by_path(classpath)  # type: ASRModel
+        logging.info(f"Restoring model : {imported_class.__name__}")
+        self.model = imported_class.restore_from(
+            restore_path=config.asr_model_path, map_location=MAP_LOCATION,
+        )
+
+        trainer = pl.Trainer(devices=DEVICE, accelerator=ACCELERATOR)
+        self.model.set_trainer(trainer)
+        self.model = self.model.eval()
+
+    ''' Main prediction function '''
+    def predict(self, filepath: str) -> str:
+
+        with torch.no_grad():
+
+            transcriptions = self.model.transcribe(
+                paths2audio_files=[filepath,],
+                batch_size=1,
+                num_workers=0,
+                return_hypotheses=False,
+            )
+
+        return transcriptions[0]
+    
+    def read_request(self, base64_string: str) -> str:
+
+        data = base64_string['data'][0]['data'].replace('data:audio/wav;base64,', '')
+        filepath = data[:15] + '.wav'
+        msg = base64.b64decode(data)
+
+        audio = np.frombuffer(msg, dtype=np.int16)
+        audio = audio.astype(np.float32, order='C') / 32767
+        sf.write(filepath, audio, 16000)
+
+        return filepath
+
+    async def __call__(self, http_request: Request) -> str:
+        
+        base64_string: str = await http_request.json()
+        filepath = self.read_request(base64_string)
+
+        text = self.predict(filepath)
+        os.remove(filepath)
+
+        return text
+
+entrypoint = Transcriber.bind()
